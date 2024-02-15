e035d48a3eb3190059233d489e4dc0cc1ba1de2d
Dario <104058513+DarioIEL@users.noreply.github.com>
2024-02-15 16:59:30 +0100

    Add files via upload

--- Lezione12-Esercizio/js/script.js
+++ Lezione12-Esercizio/js/script.js
@@ -11,12 +11,13 @@ let btnInvia = document.querySelector("#btnInvia");
 
 function salvaMessaggio(){
     let contenuto = document.querySelector("#messaggio").value;
-
     let nuovoMsg = new Messaggio("Dario", contenuto);
 
     messaggi.push(nuovoMsg);
 
     localStorage.setItem("messaggi", JSON.stringify(messaggi));
+
+    stampaMessaggi();
 }
 
 btnInvia.addEventListener("click", salvaMessaggio);
@@ -32,8 +33,33 @@ function controllaCaratteri(){
     if(caratteriRestanti <= 0){
         let contenutoMsg = document.querySelector("#messaggio").value.substring(0,10);
         textArea.value = contenutoMsg;
+        document.querySelector("#contatore").textContent = 0;
     }
     console.log(caratteriRestanti);
 }
 
 textArea.addEventListener("input", controllaCaratteri);
+
+
+///////////////////////////////////////////////
+
+let demo = document.querySelector("#demo");
+
+function stampaMessaggi (){
+    demo.innerHTML = "";
+
+    //messaggi è un Array di oggetti
+    let messaggi = JSON.parse(localStorage.getItem("messaggi"));
+
+    //msg è un oggetto {idUtente: "Dario", contenuto: "qualcosa"}
+    messaggi.forEach(msg => {
+        demo.innerHTML += `
+            <div class ='cardMsg'>
+                <h4>${msg.idUtente}:</h4>
+                <p>${msg.contenuto}</p>
+            </div>
+        `
+    });
+}
+
+//////////////////////////////////////////////////
