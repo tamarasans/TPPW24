a31162de59d1ce99d786d6a39a8f34a1ee088331
Dario <104058513+DarioIEL@users.noreply.github.com>
2024-03-07 17:49:45 +0100

    Add files via upload

--- Lezione16-Cinema/js/scriptCarrello.js
+++ Lezione16-Cinema/js/scriptCarrello.js
@@ -9,6 +9,7 @@ function recuperaFilmLS() {
     let totale = 0
 
     for (const key in localStorage) {
+
         if (Object.hasOwnProperty.call(localStorage, key)) {
 
             fetch("http://localhost:3000/spettacoli/" + key)
@@ -17,9 +18,10 @@ function recuperaFilmLS() {
                     console.log(response);
                     filmCarrello.push(response);
 
+                    // elListaRiep.innerHTML += `<li class='list-group-item'>${response.titolo} - ${response.prezzo} € </li>`;
                     totale += Number(response.prezzo);
+                    grandTotal.innerHTML = "Totale: € " + totale;
 
-                    grandTotal.innerHTML = "€ " + totale;
                 });
 
             elListaRiep.innerHTML += `<li class='list-group-item'> ${localStorage[key]} € </li>`;
@@ -34,3 +36,31 @@ function recuperaFilmLS() {
 
 
 window.addEventListener("DOMContentLoaded", recuperaFilmLS)
+
+
+
+class Persona {
+    constructor(nome, cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+
+        this.studia = function () {
+            console.log("Studia");
+        };
+    }
+}
+
+let persona = new Persona("Dario", "Menn")
+
+console.log(persona.nome);
+
+console.log(persona["nome"]); //notazione parentesi quadre
+
+for (const key in persona) {
+
+    if (Object.hasOwnProperty.call(persona, key) && typeof persona[key] != "function") {
+        const valore = persona[key];
+        console.log(valore);
+
+    }
+}
