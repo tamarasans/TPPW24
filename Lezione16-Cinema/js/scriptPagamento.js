7fdc2e94857d2aa0d61e04ccaac186d9abf3cd0e
Dario <104058513+DarioIEL@users.noreply.github.com>
2024-03-07 18:36:29 +0100

    Add files via upload

--- Lezione16-Cinema/js/scriptPagamento.js
+++ Lezione16-Cinema/js/scriptPagamento.js
@@ -1,3 +1,47 @@
 
+class Pagamento {
+    constructor(persona, email, tel, metodoPagamento, numBiglietti) {
+        this.persona = persona;
+        this.email = email;
+        this.tel = tel;
+        this.metodoPagamento = metodoPagamento;
+        this.numBiglietti = numBiglietti;
+    }
+}
 
-console.log("Carrello Script");
+function paga() {
+    let nomeUser = document.querySelector("#nomeUser").value;
+    let emailUser = document.querySelector("#emailUser").value;
+    let telUser = document.querySelector("#telUser").value;
+    let numBiglietti = document.querySelector("#numBiglietti").value;
+    let cCUser = document.querySelector("#cCUser").value;
+
+    // if(){
+    // Controlli sui singoli campi
+    // }
+
+    let pagamento = new Pagamento(nomeUser, emailUser, telUser, cCUser, numBiglietti)
+
+    fetch("http://localhost:3000/bigliettiAcquistati", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pagamento)
+    }).then(data => {
+        return data.json();
+
+    }).finally(() => {
+
+        localStorage.clear();
+        window.location.href = "./index.html"
+        alert("Pagamento andato a buon fine");
+    })
+}
+
+
+let btnPaga = document.querySelector("#btnPaga");
+
+btnPaga.addEventListener("click", paga);
+
+
